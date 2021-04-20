25fbc3ef37e3be22ca5631ab9134fe4ab8befc8f
tusharshaily <tusharshaily@gmail.com>
2021-04-21 02:15:35 +0530

    integrated the jwt with user token

--- projbacken/controllers/auth.js
+++ projbacken/controllers/auth.js
@@ -1,4 +1,13 @@
 // we can define the router action function here 
+const { check , validationResult } = require('express-validator');
+const user = require('../models/user');
+
+// for creating the token 
+var jwt = require('jsonwebtoken');
+
+// for stroing the token in the cookie 
+
+var expressJwt = require('express-jwt');
 
 exports.signout  = (req,res)=>{
 
@@ -13,6 +22,25 @@ exports.signout  = (req,res)=>{
 const User = require("../models/user");
 
 exports.signup = (req,res)=> {
+
+    const errors = validationResult(req);
+    
+    if(!errors.isEmpty())
+    {   // 422 error is useful for the database error request
+        ans = [];
+
+        errors.array().forEach( element => {
+        x = {message: element.msg , parameter :element.param }
+        ans.push(x);     
+        });
+
+        return res.status(422).json({
+
+            error: ans 
+
+        });
+    }
+
   const user = new User(req.body);
   user.save((err,user)=>{
     if(err)
@@ -29,4 +57,63 @@ exports.signup = (req,res)=> {
 
   });
 };
+
+exports.signin = (req,res) => {
+
+    const error = validationResult(req);
+
+    // error handing 
+    if(!error.isEmpty())
+    {
+        ans = [];
+
+        error.array().forEach(element =>{
+        x = {message: element.msg , parameter :element.param }
+        ans.push(x);   
+        });
+
+        return res.status(422).json({
+            error: ans
+        });
+    }
+
+
+    // we are going to find the user in the database with the given  
+
+    // we are going to destructure the data from the req.body => body-parser part
+
+    const { email , password } = req.body;
+
+    User.findOne({email}, (err,user) => {
+
+        if(err)
+        {
+            return res.status(422).json({ error : "email  not register "});
+        }
+
+        if(!user.authenticate(password))
+        {
+            return res.status(422).json({ error : "Email password didnt match"});
+        }
+
+        // token is creted 
+        var token = jwt.sign({ _id : user._id }, process.env.SECRATE);
+
+        // putting token in the cookie
+
+        res.cookie("token",token,{expire : new Date()+9999});
+        
+        // sending the response to the frontend
+
+        const {_id, name , email , role} = user ;
+        return res.json({
+            token, 
+            user : {_id,name,email,role}
+        });
+
+    });
+
+
+
+}
 
--- projbacken/routes/auth.js
+++ projbacken/routes/auth.js
@@ -1,10 +1,31 @@
 var express = require('express')
 
 var router = express.Router()
-const {signout , signup} = require("../controllers/auth");
+const {signout , signup, signin} = require("../controllers/auth");
+
+const { check , validationResult } = require('express-validator');
 
 // now we are creating a user sign up acoount 
-router.post("/signup",signup);
+
+// now we are also use express validator for the validity of the input values 
+// go  for express validator docs 
+
+router.post(
+    "/signup", 
+    check('password').isLength({min:5}).withMessage('Must be minimum length 5') ,
+    check('email').isEmail().withMessage('Need proper email'),
+    check('name').isLength({min:3}).withMessage('Name should be atleast 3 character') ,
+    signup
+    );
+
+// now we are creating the routes for the signin 
+
+router.post(
+    '/signin',
+    check('email').isEmail().withMessage("enter a valid email"),
+    check('password').isLength({min:3}).withMessage("enter a password mi length 3"),
+    signin
+);
 
 router.get("/signout", signout );
 
